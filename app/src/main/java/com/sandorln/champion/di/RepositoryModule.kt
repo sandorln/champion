006543bae261ecd441355635c22885d692c7ea77
lastkasdyu <lastkasdyu@gmail.com>
2022-04-19 04:04:38 +0900

    refact : RepositoryModule 를 @Bind 형태로 변환 및 Repository Class -> Interface 상속 형태로 변환

--- app/src/main/java/com/sandorln/champion/di/RepositoryModule.kt
+++ app/src/main/java/com/sandorln/champion/di/RepositoryModule.kt
@@ -1,32 +1,24 @@
 package com.sandorln.champion.di
 
-import com.google.gson.Gson
-import com.sandorln.champion.database.roomdao.ChampionDao
-import com.sandorln.champion.database.shareddao.VersionDao
-import com.sandorln.champion.network.ChampionService
-import com.sandorln.champion.network.VersionService
-import com.sandorln.champion.repository.BoardRepository
-import com.sandorln.champion.repository.ChampionRepository
-import com.sandorln.champion.repository.VersionRepository
-import com.sandorln.champion.repository.VersionRepositoryImpl
+import com.sandorln.champion.repository.*
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
+abstract class RepositoryModule {
     @Singleton
-    @Provides
-    fun providesChampionRepository(championService: ChampionService, championDao: ChampionDao): ChampionRepository = ChampionRepository(championService, championDao)
+    @Binds
+    abstract fun providesChampionRepository(championRepositoryImpl: ChampionRepositoryImpl): ChampionRepository
 
     @Singleton
-    @Provides
-    fun providesBoardRepository(): BoardRepository = BoardRepository()
+    @Binds
+    abstract fun providesBoardRepository(boardRepositoryImpl: BoardRepositoryImpl): BoardRepository
 
     @Singleton
-    @Provides
-    fun providesVersionRepository(versionDao: VersionDao, versionService: VersionService): VersionRepository = VersionRepositoryImpl(versionDao, versionService)
+    @Binds
+    abstract fun providesVersionRepository(versionRepositoryImpl: VersionRepositoryImpl): VersionRepository
 }
--- app/src/main/java/com/sandorln/champion/repository/ChampionRepository.kt
+++ app/src/main/java/com/sandorln/champion/repository/ChampionRepository.kt
@@ -1,48 +1,12 @@
 package com.sandorln.champion.repository
 
-import com.sandorln.champion.database.roomdao.ChampionDao
 import com.sandorln.champion.model.ChampionData
 import com.sandorln.champion.model.result.ResultData
-import com.sandorln.champion.network.ChampionService
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.withContext
 
-class ChampionRepository(
-    private val championService: ChampionService,
-    private val championDao: ChampionDao
-) {
-    suspend fun refreshAllChampionList(championVersion: String) {
-        /* DB에 해당 Champion Version 정보가 없을 시 값 갱신 */
-        val isEmptyLocalDB = championDao.getChampionList(championVersion).first().isEmpty()
-        if (isEmptyLocalDB) {
-            val response = championService.getAllChampion(championVersion)
-            response.parsingData()
-            championDao.insertChampionList(response.rChampionList)
-        }
-    }
+interface ChampionRepository {
+    suspend fun refreshAllChampionList(championVersion: String)
+    fun getAllChampionListFlow(championVersion: String): Flow<List<ChampionData>>
 
-    fun getAllChampionListFlow(championVersion: String): Flow<List<ChampionData>> = championDao.getChampionList(championVersion)
-
-    /**
-     * 특정 캐릭터 정보값 가져오기
-     */
-    private var isLoadingGetChampion = false
-    suspend fun getChampionInfo(champID: String, version: String): ResultData<ChampionData> =
-        if (!isLoadingGetChampion) {
-            try {
-                withContext(Dispatchers.IO) {
-                    isLoadingGetChampion = true
-                    val response = championService.getChampionDetailInfo(version, champID)
-                    response.parsingData()
-                    ResultData.Success(response.rChampionList.first())
-                }
-            } catch (e: Exception) {
-                ResultData.Failed(e)
-            } finally {
-                isLoadingGetChampion = false
-            }
-        } else
-            ResultData.Failed(Exception("이미 로딩중 입니다"))
+    suspend fun getChampionInfo(champID: String, version: String): ResultData<ChampionData>
 }
