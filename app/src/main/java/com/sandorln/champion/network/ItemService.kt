014520e9655f0bdb8070ecbb221330824170a4d1
lastkasdyu <lastkasdyu@gmail.com>
2022-04-20 11:32:00 +0900

    add : ItemService 추가

---
+++ app/src/main/java/com/sandorln/champion/network/ItemService.kt
@@ -0,0 +1,9 @@
+package com.sandorln.champion.network
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ItemService {
+    @GET("/cdn/{item_version}/data/ko_KR/item.json")
+    suspend fun getAllItem(@Path("item_version") itemVersion: String)
+}
