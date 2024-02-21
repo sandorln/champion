157175a049271750ab64a2bfceab99c29169c05b
dskim <lastkasdyu@gmail.com>
2024-02-21 13:55:42 +0900

    feat : champion detail screen 생성

---
+++ feature/champion/src/main/java/com/sandorln/champion/ui/detail/ChampionDetailScreen.kt
@@ -0,0 +1,18 @@
+package com.sandorln.champion.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sandorln.design.theme.LolChampionThemePreview
+
+@Composable
+fun ChampionDetailScreen() {
+
+}
+
+@Preview
+@Composable
+fun ChampionDetailScreenPreview() {
+    LolChampionThemePreview {
+        ChampionDetailScreen()
+    }
+}
