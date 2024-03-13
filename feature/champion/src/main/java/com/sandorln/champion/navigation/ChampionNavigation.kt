66c692e3fd55f9740759eb03374e05584f0f97e1
dskim <lastkasdyu@gmail.com>
2024-03-14 08:12:42 +0900

    feat : 애니메이션 처리

--- feature/champion/src/main/java/com/sandorln/champion/navigation/ChampionNavigation.kt
+++ feature/champion/src/main/java/com/sandorln/champion/navigation/ChampionNavigation.kt
@@ -1,5 +1,7 @@
 package com.sandorln.champion.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -25,7 +27,31 @@ fun NavGraphBuilder.championScreens(
         arguments = listOf(
             navArgument(BundleKeys.CHAMPION_ID) { type = NavType.StringType },
             navArgument(BundleKeys.CHAMPION_VERSION) { type = NavType.StringType }
-        )
+        ),
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                animationSpec = tween(700)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Up,
+                animationSpec = tween(700)
+            )
+        },
+        popEnterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Down,
+                animationSpec = tween(700)
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Down,
+                animationSpec = tween(700)
+            )
+        }
     ) {
         ChampionDetailScreen(
             onBackStack = onBackStack,
