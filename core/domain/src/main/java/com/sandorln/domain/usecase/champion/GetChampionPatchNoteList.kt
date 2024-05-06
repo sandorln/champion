46f05eeb3e193462446fcfe9f99dcde6e366305a
dskim <dskim@soft-berry.com>
2024-05-06 14:56:10 +0900

    feat : usecase 구현

---
+++ core/domain/src/main/java/com/sandorln/domain/usecase/champion/GetChampionPatchNoteList.kt
@@ -0,0 +1,13 @@
+package com.sandorln.domain.usecase.champion
+
+import com.sandorln.data.repository.champion.ChampionRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetChampionPatchNoteList @Inject constructor(
+    private val championRepository: ChampionRepository
+) {
+    suspend operator fun invoke(version: String) =
+        championRepository.getChampionPatchNoteList(version)
+}
