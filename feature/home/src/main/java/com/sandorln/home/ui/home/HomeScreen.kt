0aa97610a10fa7cfb2a12714163cde5fe22b486a
dskim <lastkasdyu@gmail.com>
2025-07-14 10:24:05 +0900

    feat : home bottom bar 패딩 처리

--- feature/home/src/main/java/com/sandorln/home/ui/home/HomeScreen.kt
+++ feature/home/src/main/java/com/sandorln/home/ui/home/HomeScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -103,7 +104,7 @@ fun HomeScreen(
     Scaffold(
         bottomBar = {
             if (isInitComplete) {
-                Column {
+                Column(modifier = Modifier.padding(bottom = Spacings.Spacing03)) {
                     HorizontalDivider(
                         color = Colors.Gray08,
                         thickness = 1.dp
