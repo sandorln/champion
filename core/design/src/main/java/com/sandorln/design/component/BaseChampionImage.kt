91d29629fc6b9dd8f49b972861124e5ed3aa1cfb
sandorln <lastkasdyu@gmail.com>
2026-09-12 16:50:09 +0900

    feat : BaseChampionSplashImage 로딩 원형 인디케이터(CircularProgressIndicator) 및 에러 플레이스홀더 추가

--- core/design/src/main/java/com/sandorln/design/component/BaseChampionImage.kt
+++ core/design/src/main/java/com/sandorln/design/component/BaseChampionImage.kt
@@ -1,13 +1,26 @@
 package com.sandorln.design.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.integration.compose.Transition
+import com.bumptech.glide.integration.compose.GlideSubcomposition
+import com.bumptech.glide.integration.compose.RequestState
+import com.sandorln.design.R
+import com.sandorln.design.theme.Colors
+import com.sandorln.design.theme.IconSize
 import com.sandorln.design.theme.LolChampionThemePreview
 
 @OptIn(ExperimentalGlideComposeApi::class)
@@ -20,14 +33,60 @@ fun BaseChampionSplashImage(
 ) {
     val url = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_$skinNum.jpg"
 
-    GlideImage(
+    GlideSubcomposition(
         modifier = modifier,
         model = url,
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
-        colorFilter = colorFilter
-    )
+    ) {
+        when (state) {
+            RequestState.Loading -> {
+                ChampionSplashPlaceholder()
+            }
+
+            RequestState.Failure -> {
+                ChampionSplashPlaceholder(isError = true)
+            }
+
+            is RequestState.Success -> {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painter,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    colorFilter = colorFilter
+                )
+            }
+        }
+    }
 }
+
+@Composable
+fun ChampionSplashPlaceholder(
+    modifier: Modifier = Modifier,
+    isError: Boolean = false
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Colors.Gray09),
+        contentAlignment = Alignment.Center
+    ) {
+        if (isError) {
+            Icon(
+                modifier = Modifier.size(IconSize.LargeSize),
+                painter = painterResource(id = R.drawable.ic_main_champion),
+                contentDescription = null,
+                tint = Colors.Gray06
+            )
+        } else {
+            CircularProgressIndicator(
+                modifier = Modifier.size(IconSize.LargeSize),
+                color = Colors.Gold03,
+                strokeWidth = 2.dp
+            )
+        }
+    }
+}
+
 
 @Preview
 @Composable
